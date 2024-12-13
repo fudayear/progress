d8b353b4abc85f1b4c43b9f528eec51b2b4c2110
amonnagasawa@gmail.com <=>
2024-12-13 13:51:46 +0900

    bootstrapの導入

--- your_progress/src/main.ts
+++ your_progress/src/main.ts
@@ -1,11 +1,14 @@
 import './style.css';
 import { supabase } from './supabase';
 
-(async () => {
+const testGet = async () => {
   const { data, error } = await supabase.from('main_texts').select('*');
   if (error) {
     console.error('Error:', error.message);
   } else {
     console.log('Data:', data);
   }
-})();
+};
+
+testGet();
+
