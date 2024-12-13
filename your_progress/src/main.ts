a405f6074815c72aec3df83dfc59b0e896a3e64d
fudayear <amonnagasawa@gmail.com>
2024-12-13 22:25:46 +0900

    formを用意し、疑似SQLを書き始めた　addEventListenerをなんとかする

--- your_progress/src/main.ts
+++ your_progress/src/main.ts
@@ -1,5 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import { supabase } from './supabase';
+
+const main_texts = {
+
+}
 
 const testGet = async () => {
   const { data, error } = await supabase.from('main_texts').select('*');
@@ -10,5 +15,15 @@ const testGet = async () => {
   }
 };
 
+const post = async (massage: string, author: string) => {
+  const error = await supabase.from('main_texts').insert([{
+    content: massage,
+    author: author,
+  }]);
+}
 testGet();
 
+const form = document.querySelector('form') as HTMLFormElement;
+const content = document.querySelector('[id = "progress"]') as HTMLDivElement;
+
+form.addEventListener('submit', async (e) => {
