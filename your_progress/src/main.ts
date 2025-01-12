ab952cba8e4d4eb50b5306d1996b21e06564a9eb
fudayear <amonnagasawa@gmail.com>
2025-01-12 20:44:06 +0900

    ページレイアウトの決定、progressの取得（not worked）

--- your_progress/src/main.ts
+++ your_progress/src/main.ts
@@ -3,9 +3,10 @@ import './style.css';
 import { supabase } from './supabase';
 
 const main_texts = {
-
+  //supabase内のmain_textsの中身を入れる
 }
 
+//アクセス時に自動的に呼び出される
 const testGet = async () => {
   const { data, error } = await supabase.from('main_texts').select('*');
   if (error) {
@@ -14,16 +15,44 @@ const testGet = async () => {
     console.log('Data:', data);
   }
 };
-
+//テキストを送る
 const post = async (massage: string, author: string) => {
   const error = await supabase.from('main_texts').insert([{
     content: massage,
     author: author,
   }]);
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log("Data posted: ", massage, author);
+  }
 }
-testGet();
+//表示テキストを取得
+//なぜか動かない
+const progresGet = async () => {
+  const { data: mainTexts, error } = await supabase
+    .from('main_texts')
+    .select('*')
+    .order('id', { ascending: false })
+    .limit(10);
+
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(mainTexts);
+  }
+}
+
+//ここから上は関数
+//ここから下はページが読み込まれた時に呼び出される、main関数のようなもの
+progresGet();
 
 const form = document.querySelector('form') as HTMLFormElement;
 const content = document.querySelector('[id = "progress"]') as HTMLDivElement;
 
 form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const formData = new FormData(form);
+
+})
